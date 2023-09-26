4e828fa9ff023d7c7c7a01e1f3eb1834d71c641b
Tomáš Holata <tomas.holata@gmail.com>
2023-09-26 14:37:12 +0200

    Oprava vypisování výsledku aby nebyla jen remíza.

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -4,14 +4,14 @@ namespace RockPaperScissors
 {
     internal class Program
     {
-        int scorePc = 0; // skóre počítače
+        int scorePC = 0; // skóre počítače
         int scorePlayer = 0; // skóre hráče
         string item = ""; // předmět hráče string
-        int itemPc = 0; // předmět PC
+        int itemPC = 0; // předmět PC
         int itemPlayer = 0; // předmět hráče
         Random rng = new Random(); // instance třídy Random pro generování náhodných čísel
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Program function = new Program();
             Console.WriteLine("Vítej ve hře. Stiskni klávesu pro začátek hry");
@@ -19,13 +19,13 @@ namespace RockPaperScissors
             function.Hra();
         }
 
-        public void Hra()
+        public int Hra()
         {
             Program function = new Program();
 
-            while (true) // Změna podmínky na nekonečnou smyčku
+            while (true) 
             {
-                item = ""; // Resetujeme volbu hráče pro každé kolo
+                item = "";
 
                 Console.WriteLine("Pro nůžky zadej N, pro kámen K, pro papír P");
                 item = Console.ReadLine();
@@ -46,13 +46,13 @@ namespace RockPaperScissors
                         itemPlayer = 3;
                         break;
                     default:
-                        Console.WriteLine("Toto není platná volba");
-                        continue; // Pokračujeme ve smyčce, abychom získali platný vstup
+                        Console.WriteLine("Toto není platná volba"); // Když není ani jedno písmeno správné
+                        continue;
                 }
 
-                itemPc = rng.Next(1, 4); // 1=kámen, 2=nůžky, 3=papír
+                itemPC = rng.Next(1, 4); // 1=kámen, 2=nůžky, 3=papír
 
-                switch (itemPc)
+                switch (itemPC)  // Vypsání předmětu PC 
                 {
                     case 1:
                         Console.WriteLine("PC dává kámen");
@@ -65,31 +65,24 @@ namespace RockPaperScissors
                         break;
                 }
 
-                function.result();
+                if (itemPC == itemPlayer)   // Výpočítání výsledku
+                {
+                    Console.WriteLine("Remíza");
+                }
+                else if ((itemPlayer == 1 && itemPC == 2) || (itemPlayer == 2 && itemPC == 3) || (itemPlayer == 3 && itemPC == 1))
+                {
+                    Console.WriteLine("Hráč vyhrál");
+                }
+                else
+                {
+                    Console.WriteLine("Počítač vyhrál");
+                }
 
-                Console.WriteLine("Aktuální skóre hráče je: " + scorePlayer + Environment.NewLine + "Počítače: " + scorePc);
+                Console.WriteLine("Aktuální skóre hráče je: " + scorePlayer + Environment.NewLine + "Počítače: " + scorePC);
                 Console.WriteLine("Pro restart hry zmáčkni libovolné tlačítko");
                 Console.ReadKey();
                 Console.Clear();
             }
-        }
-
-        public void result()
-        {
-            if (itemPc == itemPlayer)
-            {
-                Console.WriteLine("Remíza!");
-            }
-            else if ((itemPlayer == 1 && itemPc == 2) || (itemPlayer == 2 && itemPc == 3) || (itemPlayer == 3 && itemPc == 1))
-            {
-                Console.WriteLine("Hráč vyhrál!");
-                scorePlayer++;
-            }
-            else if ((itemPc == 1 && itemPlayer == 2) || (itemPc == 2 && itemPlayer == 3) || (itemPc == 3 && itemPlayer == 1))
-            {
-                Console.WriteLine("Počítač vyhrál!");
-                scorePc++;
-            }
-        }
+        }     
     }
 }
