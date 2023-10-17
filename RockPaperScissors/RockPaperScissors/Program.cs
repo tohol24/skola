12fbcc0f6c4cc5a448837fc7ac2518d963af7800
Tomas Holata <tomas.holata@gmail.com>
2023-10-17 11:21:20 +0200

    Updates

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -4,90 +4,23 @@ namespace RockPaperScissors
 {
     internal class Program
     {
-        int scorePC = 0; // skóre počítače
-        int scorePlayer = 0; // skóre hráče
-        string item = ""; // předmět hráče string
-        int itemPC = 0; // předmět PC
-        int itemPlayer = 0; // předmět hráče
-        Random rng = new Random(); // instance třídy Random pro generování náhodných čísel
-
         private static void Main(string[] args)
         {
-            Program function = new Program();
+            int scorePC = 0;
+            int scorePlayer = 0;
+            Random rng = new Random();
+
             Console.WriteLine("Vítej ve hře. Stiskni klávesu pro začátek hry");
-            Console.WriteLine(); 
+            Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Green;
             Console.ReadKey();
-            function.Hra();
-        }
-
-        public int Hra()
-        {
-            Program function = new Program();
 
             while (true)
             {
-                item = "";
+                int itemPlayer = GetitemPlayer();
+                int itemPC = GetitemPC(rng);
 
-                Console.WriteLine("Pro nůžky zadej N, pro kámen K, pro papír P");
-                item = Console.ReadLine();
-                item = item.ToLower();
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Red;
-
-                switch (item)
-                {
-                    case "k":
-                        Console.WriteLine("Zvolil jsi kámen");
-                        itemPlayer = 1;
-                        break;
-                    case "n":
-                        Console.WriteLine("Zvolil jsi nůžky");
-                        itemPlayer = 2;
-                        break;
-                    case "p":
-                        Console.WriteLine("Zvolil jsi papír");
-                        itemPlayer = 3;
-                        break;
-                    default:
-                        Console.WriteLine("Toto není platná volba"); // Když není ani jedno písmeno správné
-                        continue;
-                }
-
-                itemPC = rng.Next(1, 4); // 1=kámen, 2=nůžky, 3=papír
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Yellow;
-
-                switch (itemPC)  // Vypsání předmětu PC 
-                {
-                    case 1:
-                        Console.WriteLine("PC dává kámen");
-                        break;
-                    case 2:
-                        Console.WriteLine("PC dává nůžky");
-                        break;
-                    case 3:
-                        Console.WriteLine("PC dává papír");
-                        break;
-                }
-
-                Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.Green;
-
-                if (itemPC == itemPlayer)   // Výpočítání výsledku
-                {
-                    Console.WriteLine("!REMÍZA!");
-                }
-                else if ((itemPlayer == 1 && itemPC == 2) || (itemPlayer == 2 && itemPC == 3) || (itemPlayer == 3 && itemPC == 1))
-                {
-                    Console.WriteLine("Hráč vyhrál");
-                    scorePlayer++;
-                }
-                else
-                {
-                    Console.WriteLine("Počítač vyhrál");
-                    scorePC++;
-                }
+                GameResult(itemPlayer, itemPC, ref scorePlayer, ref scorePC);
 
                 Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.Cyan;
@@ -96,6 +29,66 @@ namespace RockPaperScissors
                 Console.WriteLine("Pro další hru zmáčkni libovolné tlačítko");
                 Console.ReadKey();
                 Console.Clear();
+            }
+        }
+        private static int GetitemPlayer()
+        {
+            Console.WriteLine("Pro nůžky zadej N, pro kámen K, pro papír P");
+            string input = Console.ReadLine().ToLower();
+            switch (input)
+            {
+                case "k":
+                    Console.WriteLine("Zvolil jsi kámen");
+                    return 1;
+                case "n":
+                    Console.WriteLine("Zvolil jsi nůžky");
+                    return 2;
+                case "p":
+                    Console.WriteLine("Zvolil jsi papír");
+                    return 3;
+                default:
+                    Console.WriteLine("Toto není platná volba");
+                    return GetitemPlayer();
+            }
+        }
+
+        private static int GetitemPC(Random rng)
+        {
+            return rng.Next(1, 4);
+        }
+
+        private static void GameResult(int itemPlayer, int itemPC, ref int scorePlayer, ref int scorePC) // Tuto funkci pomohl udělat GPT
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            switch (itemPC)
+            {
+                case 1:
+                    Console.WriteLine("PC dává kámen");
+                    break;
+                case 2:
+                    Console.WriteLine("PC dává nůžky");
+                    break;
+                case 3:
+                    Console.WriteLine("PC dává papír");
+                    break;
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            if (itemPlayer == itemPC)
+            {
+                Console.WriteLine("!REMÍZA!");
+            }
+            else if ((itemPlayer + 1) == itemPC || (itemPlayer == 3 && itemPC == 1))
+            {
+                Console.WriteLine("Hráč vyhrál");
+                scorePlayer++;
+            }
+            else
+            {
+                Console.WriteLine("Počítač vyhrál");
+                scorePC++;
             }
         }
     }
