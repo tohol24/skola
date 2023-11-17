76c0ff02eccd69aa4d63470f20835351c535b041
Tomáš Holata <tomas.holata@gmail.com>
2023-11-17 16:40:25 +0100

    Přenos úkolu z domova

--- 2D Array Playground/2D Array Playground/Program.cs
+++ 2D Array Playground/2D Array Playground/Program.cs
@@ -12,72 +12,122 @@ namespace _2D_Array_Playground
         static void Main(string[] args)
         {
             //TODO 1: Vytvoř integerové 2D pole velikosti 5 x 5, naplň ho čísly od 1 do 25 a vypiš ho do konzole (5 řádků po 5 číslech).
-            int[,] board = new int[5, 5];
+            int[,] myArray = CreateBoard();
+            PrintBoard(myArray);
+
+            //TODO 2: Vypiš do konzole n-tý řádek pole, kde n určuje proměnná nRow.
+            int nRow = GetInputRow();
+            PrintRow(myArray, nRow);
+
+            //TODO 3: Vypiš do konzole n-tý sloupec pole, kde n určuje proměnná nColumn.
+            int nColumn = GetInputColumn();
+            PrintColumn(myArray, nColumn);
+
+            //TODO 4: Vypiš hlavní diagonálu pole.
+            PrintMainDiagonal(myArray);
+
+            //TODO 5: Vypiš vedlejší diagonálu pole.
+            PrintSecondaryDiagonal(myArray);
+        }
+
+        private static int[,] CreateBoard()
+        {
+            int[,] myArray = new int[5, 5];
             int next = 1;
-            for (int i = 0; i < board.GetLength(0); i++)
+            for (int i = 0; i < myArray.GetLength(0); i++)
             {
-                for (int j = 0; j < board.GetLength(1); j++)
+                for (int j = 0; j < myArray.GetLength(1); j++)
                 {
-                    board[i, j] = next;
-                    Console.Write(board[i, j] + " ");
+                    myArray[i, j] = next;
                     next++;
+                }
+            }
+            return myArray;
+        }
+
+        private static void PrintBoard(int[,] myArray)
+        {
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.Write(myArray[i, j] + " ");
                 }
                 Console.WriteLine();
             }
+        }
 
-            //TODO 2: Vypiš do konzole n-tý řádek pole, kde n určuje proměnná nRow.
-
+        private static int GetInputRow()
+        {
             Console.WriteLine("Zadej řádek k vyhledání:");
             int nRow = Convert.ToInt32(Console.ReadLine());
+            return nRow;
+        }
+
+        private static void PrintRow(int[,] myArray, int nRow)
+        {
             Console.WriteLine("Nyní vypíši řádek: " + nRow);
-            for (int j = 0; j < board.GetLength(0); j++)
+            for (int j = 0; j < myArray.GetLength(0); j++)
             {
-                Console.Write(board[nRow, j] + " ");
+                Console.Write(myArray[nRow, j] + " ");
             }
             Console.WriteLine();
+        }
 
-            //TODO 3: Vypiš do konzole n-tý sloupec pole, kde n určuje proměnná nColumn.
+        private static int GetInputColumn()
+        {
             Console.WriteLine("Zadej SLOUPEC k vyhledání:");
-            int nColumn = Convert.ToInt32(Console.ReadLine()); ;
-            Console.WriteLine("Nyní vypíši řádek: " + nRow);
-            for (int i = 0; i < board.GetLength(1); i++)
+            int nColumn = Convert.ToInt32(Console.ReadLine());
+            return nColumn;
+        }
+
+        private static void PrintColumn(int[,] myArray, int nColumn)
+        {
+            Console.WriteLine("Nyní vypíši sloupec: " + nColumn);
+            for (int i = 0; i < myArray.GetLength(1); i++)
             {
-                Console.WriteLine(board[i, nColumn] + " ");
+                Console.WriteLine(myArray[i, nColumn] + " ");
             }
             Console.WriteLine();
+        }
 
-            for (int i = 0; i < board.GetLength(0); i++)
+        private static void PrintMainDiagonal(int[,] myArray)
+        {
+            Console.WriteLine("Hlavní diagonala: ");
+            for (int i = 0; i < myArray.GetLength(0); i++)
             {
-                Console.Write(board[i, i]);
+                Console.Write(myArray[i, i] + " ");
             }
-            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine();
-            Console.WriteLine("Reverse: ");
-            for (int i = 4; i >= 0; i--)
+        }
+
+        private static void PrintSecondaryDiagonal(int[,] myArray)
+        {
+            Console.WriteLine("Vedlejší diagonala: ");
+            for (int i = 0; i < myArray.GetLength(0); i++)
             {
-                Console.Write(board[i, board.GetLength(0) - 1]);
+                Console.Write(myArray[i, myArray.GetLength(0) - 1 - i] + " ");
             }
-            Console.ForegroundColor = ConsoleColor.White;
-
-            //TODO 4: Prohoď prvek na souřadnicích [xFirst, yFirst] s prvkem na souřadnicích [xSecond, ySecond] a vypiš celé pole do konzole po prohození.
-            //Nápověda: Budeš potřebovat proměnnou navíc, do které si uložíš první z prvků před tím, než ho přepíšeš druhým, abys hodnotou prvního prvku potom mohl přepsat druhý
-            int xFirst, yFirst, xSecond, ySecond;
-
-            //TODO 5: Prohoď n-tý řádek v poli s m-tým řádkem (n je dáno proměnnou nRowSwap, m mRowSwap) a vypiš celé pole do konzole po prohození.
-            int nRowSwap = 0;
-            int mRowSwap = 1;
-
-            //TODO 6: Prohoď n-tý sloupec v poli s m-tým sloupcem (n je dáno proměnnou nColSwap, m mColSwap) a vypiš celé pole do konzole po prohození.
-            int nColSwap = 0;
-            int mColSwap = 1;
-
-            //TODO 7: Otoč pořadí prvků na hlavní diagonále (z levého horního rohu do pravého dolního rohu) a vypiš celé pole do konzole po otočení.
-
-
-            //TODO 8: Otoč pořadí prvků na vedlejší diagonále (z pravého horního rohu do levého dolního rohu) a vypiš celé pole do konzole po otočení.
-
-
+            Console.WriteLine();
             Console.ReadKey();
         }
+
+        //TODO 4: Prohoď prvek na souřadnicích [xFirst, yFirst] s prvkem na souřadnicích [xSecond, ySecond] a vypiš celé pole do konzole po prohození.
+        //Nápověda: Budeš potřebovat proměnnou navíc, do které si uložíš první z prvků před tím, než ho přepíšeš druhým, abys hodnotou prvního prvku potom mohl přepsat druhý
+        int xFirst, yFirst, xSecond, ySecond;
+
+        //TODO 5: Prohoď n-tý řádek v poli s m-tým řádkem (n je dáno proměnnou nRowSwap, m mRowSwap) a vypiš celé pole do konzole po prohození.
+        int nRowSwap = 0;
+        int mRowSwap = 1;
+
+        //TODO 6: Prohoď n-tý sloupec v poli s m-tým sloupcem (n je dáno proměnnou nColSwap, m mColSwap) a vypiš celé pole do konzole po prohození.
+        int nColSwap = 0;
+        int mColSwap = 1;
+
+        //TODO 7: Otoč pořadí prvků na hlavní diagonále (z levého horního rohu do pravého dolního rohu) a vypiš celé pole do konzole po otočení.
+
+
+        //TODO 8: Otoč pořadí prvků na vedlejší diagonále (z pravého horního rohu do levého dolního rohu) a vypiš celé pole do konzole po otočení.
+
     }
 }
---
+++ 2DArray/2DArray/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+
+namespace _2DArray
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            try
+            {
+                defColor();
+                short menu = 0; // Proměná pro volbu menu
+                int[,] myArray; // Hlavní matice
+                int[,] backArray; // Záloha původní vygenerované matice
+                myArray = CreateArray();
+                backArray = myArray;
+                WriteArray(myArray);
+                Functions(menu, myArray, backArray);
+            }
+            catch (Exception)
+            {
+                Error();
+            }
+        }
+
+        static void Functions(short menu, int[,] myArray, int[,] backArray)  // Menu pro uživatele
+        {
+            while (true)
+            {
+                try
+                {
+                    defColor();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Zvol funkci, kterou chceš. Napiš:\n1 pro sečtení vygenerované (původní) matice\n2 pro vynásobení součtu prvků kvocientem\n3 pro vynásobení součtem prvků jiné matice\n4 pro přehození čísel na hlavní diagonále\n5 pro překlopení čísel přes hlavní diagonálu");
+                    menu = Convert.ToInt16(Console.ReadLine());
+                    defColor();
+                    switch (menu)
+                    {
+                        case 1:
+                            int sum = sumArray(myArray);
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Součet matice je {sum}:");
+                            break;
+                        case 2:
+                            multiplyArray(myArray);
+                            break;
+                        case 3:
+                            arrayTimesArray(myArray, backArray);
+                            break;
+                        case 4:
+                            swapMain(myArray, backArray);
+                            break;
+                        case 5:
+                            transposeArray(myArray, backArray);
+                            break;
+                        default:
+                            Console.WriteLine("Bohužel toto ještě neumím, hezký den!");
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Error();
+                }
+
+            }
+        }
+
+        static int[,] CreateArray() // Menu pro vytvoření matice matice
+        {
+            try
+            {
+                Console.WriteLine($"Napiš:\n1 pro posloupnou matici\n2 pro náhodnou matici");
+                short menu = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Nyní zadej délku strany čtvercové matice:");
+                int size = Convert.ToInt32(Console.ReadLine());
+                clearConsole();
+                switch (menu)
+                {
+                    case 1:
+                        return orderedArray(size);
+                    case 2:
+                        return randArray(size);
+                    default:
+                        Error();
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+
+                Error();
+            }
+            return null;
+        }
+
+        static int[,] randArray(int size) // Vytvoření náhodné matice
+        {
+            Random random = new Random();
+            int[,] myArray = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    myArray[i, j] = random.Next(1, 100);
+                }
+            }
+            return myArray;
+        }
+
+        static int[,] orderedArray(int size) // Vytvoření posloupné matice
+        {
+            try
+            {
+                int num = 1;
+                int[,] myArray = new int[size, size];
+
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        myArray[i, j] = num;
+                        num++;
+                    }
+                }
+                return myArray;
+            }
+            catch (Exception)
+            {
+                Error();
+                return null;
+            }
+        }
+
+        static void WriteArray(int[,] myArray) // Vypsání matice
+        {
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.Write(myArray[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int sumArray(int[,] myArray) // Součet členů matice
+        {
+            int sum = 0;
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    sum += myArray[i, j];
+                }
+            }
+            return sum;
+        }
+
+        static void multiplyArray(int[,] myArray)  // Násobení součtu matice kvocientem
+        {
+            try
+            {
+                Console.WriteLine("Zadej kvocient, kterým chceš matici násobit");
+                int quotient = Convert.ToInt32(Console.ReadLine());
+                int result = sumArray(myArray) * quotient;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Násobení součtu matice číslem {quotient} je {result}:");
+            }
+            catch (Exception)
+            {
+                Error();
+            }
+        }
+
+        static void arrayTimesArray(int[,] myArray, int[,] backArray) // Funkce násobení součtu dvou matic
+        {
+            int sum1 = 0;
+            int sum2 = 0;
+            int result = 0;
+            sum1 = sumArray(backArray);
+            myArray = CreateArray();
+            sum2 = sumArray(myArray);
+            result = sum1 * sum2;
+            myArray = backArray;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Součin původní matice a nové matice je {result}");
+        }
+
+        static void swapMain(int[,] myArray, int[,] backArray) //Přehození číslel na hlavní diagonále
+        {
+            for (int i = 0; i < myArray.GetLength(0) / 2; i++)
+            {
+                int tempMain = myArray[i, i];
+                int reversed = myArray.GetLength(0) - 1;
+                myArray[i, i] = myArray[reversed, reversed];
+                myArray[reversed, reversed] = tempMain;
+            }
+            WriteArray(myArray);
+            myArray = backArray;
+            Console.WriteLine();
+        }
+
+        static void transposeArray(int[,] myArray, int[,] backArray)  // Transpozice matice přes hlavní diagonálu
+        {
+            int[,] tempArray = new int[myArray.GetLength(0), myArray.GetLength(0)];
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    tempArray[i, j] = myArray[j, i];
+                }
+            }
+            myArray = tempArray;
+            WriteArray(myArray);
+            myArray = backArray;
+        }
+
+        static void Error()  // Funkce pro chybu (Catch)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Byla zadána neplatná hodnota, program se restartuje! Stisktni libovolnou klávesu.");
+            Console.ReadKey();
+            Main();
+            clearConsole();
+        }
+
+        static void clearConsole() //Vyčištění konzole
+        {
+            Console.Clear();
+        }
+
+        static void defColor() // Nastavení původní barvy pozadí a textu
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
